43098af5f25b77c47c7d86993007dcec44db1118
Jonathan Li <jli7733>
2023-09-14 12:05:44 -0400

    added react app

---
+++ animanga-list/src/components/Card.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './../App.css';
+
+export interface CardProps{
+    animeName: string
+    imageURL: string
+    animeURL: string
+}
+
+function Card(props: CardProps){
+    const {animeName, imageURL, animeURL} = props
+    return (
+        <div className = "Card">
+            <div className = "animeTitle">
+                <div className="titleText">
+                    <h2><a className = "animeLink" href={animeURL}>{animeName}</a></h2>
+                </div>
+            </div>
+            <div className = "Image">
+                <img className = "rounded-image" src={imageURL} ></img>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
