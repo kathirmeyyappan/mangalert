fee177398f37fec34b9e97a6ecf00476631cb260
Jonathan Li <jli7733>
2023-09-14 14:37:29 -0400

    modifying fetch function

--- animanga-list/src/App.tsx
+++ animanga-list/src/App.tsx
@@ -2,36 +2,37 @@ import * as React from 'react';
 import './App.css';
 import Card from './components/Card'
 
-// use this format to make calls to backend (once you've made the express.get in there)
-fetch('/api/userLogin', {
-  method: 'GET'
-})
-.then((response) => response.json())
-.then((data) => {
-  // Handle the data received from the server
-  console.log(data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-
 function App() {
-const mockAnime = {
-    "anime": [
-        {"name" : "Testing the wrapping of this text if i make this really fucking long holy shit like even longer than this",
-         "img_url": "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
-          "url": "https://myanimelist.net/anime/1/"},
-        {"name" : "Cowboy Bebop",
+  const userid = "9025957"
+
+  fetch(`/getFinishedPlannedAnime/userid`, {
+    method: 'GET',
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    // Handle the data received from the server
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+
+  const mockAnime = {
+      "anime": [
+          {"name" : "Testing the wrapping of this text if i make this really fucking long holy shit like even longer than this",
           "img_url": "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
-           "url": "https://myanimelist.net/anime/1/"},
-        {"name" : "Cowboy Bebop",
-        "img_url": "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
-        "url": "https://myanimelist.net/anime/1/"},
-        {"name" : "Cowboy Bebop",
-        "img_url": "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
-         "url": "https://myanimelist.net/anime/1/"},
-    ]
-}
+            "url": "https://myanimelist.net/anime/1/"},
+          {"name" : "Cowboy Bebop",
+            "img_url": "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
+            "url": "https://myanimelist.net/anime/1/"},
+          {"name" : "Cowboy Bebop",
+          "img_url": "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
+          "url": "https://myanimelist.net/anime/1/"},
+          {"name" : "Cowboy Bebop",
+          "img_url": "https://cdn.myanimelist.net/images/anime/4/19644.jpg",
+          "url": "https://myanimelist.net/anime/1/"},
+      ]
+  }
 
   return (
     <div className="App">
