20ee74fcaf196c9b0b7719eca996c12cacc17cc1
kathirmeyyappan <kathirmey@gmail.com>
2023-09-14 20:17:56 -0400

    react application is working * added support for user without any plan-to-watch content * fixed reload error (I think) * changed structure of index.js in react to account for async anime and manga * fixed server call to get user id from react app location * other stuff I cna't remember

--- animanga-list/src/index.js
+++ animanga-list/src/index.js
@@ -7,43 +7,45 @@ import React from "react";
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
-const userid = window.location.pathname.split('/')[-1];
+function renderApp(anime, manga) {
+  console.log(anime)
+  root.render(
+    <StrictMode>
+      <App anime={anime} manga={manga} />
+    </StrictMode>,
+    rootElement
+  );
+}
 
-let anime;
-let manga;
 
-// Fetch finished planned anime json
-fetch(`/getFinishedPlannedAnime`, {
-  method: 'GET',
-  header: {
-    userid: userid
-  }
-})
-.then((response) => response.json())
-.then((data) => {
-  anime = data
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+function fetchDataAndRenderApp() {
+  let anime;
+  let manga;
+  // fetch finished planned anime json
+  fetch('/getFinishedPlannedAnime', { method: 'GET' })
+    .then((response) => response.json())
+    .then((data) => {
+      anime = data;
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    })
+    .finally(() => {
+      // Fetch finished planned manga json
+      fetch(`/getFinishedPlannedManga`, { method: 'GET' })
+        .then((response) => response.json())
+        .then((data) => {
+          manga = data;
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        })
+        .finally(() => {
+          // Once both data sets are fetched, render the app
+          renderApp(anime, manga);
+        });
+    });
+}
 
-// Fetch finished planned manga json
-fetch(`/getFinishedPlannedManga`, {
-  method: 'GET',
-  headers: {
-    userid: userid
-  }
-})
-.then((response) => response.json())
-.then((data) => {
-  manga = data
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
 
-root.render(
-  <StrictMode>
-    <App anime = {anime} manga = {anime}/>
-  </StrictMode>
-);
+fetchDataAndRenderApp();
